179d041b9015b3c64af5e26ee087165cb2066a10
arsndirio <rioarisandi201@gmail.com>
2024-10-23 17:02:27 +0700

    tugas 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
                     </svg>
                 </div>
 
-                
+                <h1 style="font-size: 48px; color: #ffffff; text-align: center;">Rio Arisandi - 2217051154</h1>
 
                 <div class="mt-16">
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
